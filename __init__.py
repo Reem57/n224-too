16275d93db6651ac6aab121ee188f301cdca0b35
nicm2 <89167131+nicm2@users.noreply.github.com>
2022-05-12 22:17:38 -0700

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+
+
+app = Flask(__name__)
+dbURI = 'sqlite:///model/myDB.db'
+# Setup properties for the database
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = dbURI
+app.config['SECRET_KEY'] = 'SECRET_KEY'
+db = SQLAlchemy(app)
+Migrate(app, db)
+login_manager = LoginManager()
+login_manager.init_app(app)
