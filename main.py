5055fca867539440ada70edef036b8bc1573d94e
Reem57 <benishai.r@gmail.com>
2022-05-17 14:23:10 -0700

    login

--- main.py
+++ main.py
@@ -26,8 +26,8 @@ def register():
 
 
 @app.route('/login/')
-def login():
-    return render_template("login.html")
+def register():
+    return render_template("cruddy/templates/login.html")
 
  
 # runs the application on the development server
