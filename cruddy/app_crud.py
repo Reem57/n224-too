09764e1962d16934bdf24088cbed5eb4491d39ea
Reem57 <benishai.r@gmail.com>
2022-05-17 13:37:50 -0700

    login

--- cruddy/app_crud.py
+++ cruddy/app_crud.py
@@ -2,7 +2,7 @@
 from flask import Blueprint, render_template, request, url_for, redirect, jsonify, make_response
 from flask_login import login_required, logout_user
 
-from cruddy.query import *
+from cruddy1.query import *
 
 # blueprint defaults https://flask.palletsprojects.com/en/2.0.x/api/#blueprint-objects
 app_crud = Blueprint('crud', __name__,
