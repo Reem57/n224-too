04aaf68e78954325f5653fec6759d3750a5c0d68
nicm2 <89167131+nicm2@users.noreply.github.com>
2022-05-23 13:14:00 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
-# import "packages" from flask
-import requests
+from flask import Flask, render_template
 from __init__ import app
-from flask import Flask, request, Blueprint, render_template
 from cruddy.app_crud import app_crud
-# rom cruddy.app_crud_api import app_crud_api
-from userNotes import userNotes
+from cruddy.app_crud_api import app_crud_api
+from cruddy.app_notes import app_notes
+import markdown
 
 app = Flask(__name__)
 
+
 app.register_blueprint(app_crud)
-# app.register_blueprint(app_crud_api)
-app.register_blueprint(userNotes)
-# connects default URL to render index.html
+app.register_blueprint(app_crud_api)
+app.register_blueprint(app_notes)
 
 
 
@@ -24,14 +23,6 @@ def index():
 # def stub():
 #     return render_template("simple_calc.html")
 
-@app.route('/register/')
-def register():
-    return render_template("register.html")
-
-
-@app.route('/login/')
-def login():
-    return render_template("login.html")
 
 # runs the application on the development server
 if __name__ == "__main__":
